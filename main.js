357adb1a1567a65f529851fb69b0aeb97a27dd14
Rosario Di Chiara <dchross97@outlook.it>
2025-01-07 20:25:58 +0100

    selfwork DOM_1

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 
+let paragrafi = document.querySelectorAll('p')
+let cambiaColore = document.querySelector('#cambiaColore')
+let rendiGrassetto = document.querySelector('#rendiGrassetto')
+let nascondiMostra = document.querySelector('#nascondiMostra')
+
+
+cambiaColore.addEventListener('click', ()=>{
+    paragrafi.forEach(paragrafo => {
+      let r = Math.floor(Math.random()* 256)
+      let g = Math.floor(Math.random()* 256)
+      let b = Math.floor(Math.random()* 256)
+      paragrafo.style.color = `rgb(${r}, ${g}, ${b})`
+    })
+});
+
+rendiGrassetto.addEventListener('click', ()=>{
+    paragrafi.forEach(paragrafo =>{
+        paragrafo.style.fontWeight = `bold`
+    })
+});
+
+nascondiMostra.addEventListener('click', ()=>{
+    paragrafi.forEach(paragrafo =>{
+       if(paragrafo.style.display == `block`){
+        paragrafo.style.display = `none`
+       } else{
+        paragrafo.style.display = `block`
+       }
+    })
+})
+
+
+
